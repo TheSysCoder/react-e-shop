99b80aa3280a1a9bf7601d00e6a27ca6f84d171e
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-06-28 21:18:52 +0530

    Component Done

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ const App = () => {
     },
     {
       id: 4,
-      title: "Mens",
+      title: "Womens",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "Womens",
+      title: "Mens",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
