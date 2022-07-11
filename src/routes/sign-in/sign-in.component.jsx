b7cbd71ce17d6ee50e78de018e4686c96d86bda5
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-07-11 12:21:34 +0530

    sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,29 @@
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocfromAuth,
+  signWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
-
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     createUserDocfromAuth(user);
     const userDocRef = await createUserDocfromAuth(user);
   };
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signWithGoogleRedirect();
+    console.log(user);
+  };
 
   return (
     <div>
       <h1>Sign IN</h1>
       <button onClick={logGoogleUser}>Sign with Google</button>
+      <button onClick={signWithGoogleRedirect}>
+        Sign with Google Redirect
+      </button>
     </div>
   );
 };
